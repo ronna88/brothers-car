799669d6bf45162cfbb3a9e57ca9ade421d2308c
Theo <ronna88@gmail.com>
2024-09-14 21:19:50 -0300

    chore: add prisma setup

---
+++ prisma/migrations/20240915001410_init_db/migration.sql
@@ -0,0 +1,76 @@
+-- CreateEnum
+CREATE TYPE "TipoUsuario" AS ENUM ('cliente', 'administrativo');
+
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('pendente', 'aprovado', 'pago', 'cancelado');
+
+-- CreateTable
+CREATE TABLE "Usuario" (
+    "id" SERIAL NOT NULL,
+    "nome" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "senha" TEXT NOT NULL,
+    "cpf" TEXT,
+    "telefone" TEXT,
+    "endereco" TEXT,
+    "tipo_usuario" "TipoUsuario" NOT NULL,
+    "data_criacao" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Usuario_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Carro" (
+    "id" SERIAL NOT NULL,
+    "cliente_id" INTEGER NOT NULL,
+    "marca" TEXT NOT NULL,
+    "modelo" TEXT NOT NULL,
+    "ano" INTEGER NOT NULL,
+    "placa" TEXT NOT NULL,
+    "data_registro" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Carro_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Orcamento" (
+    "id" SERIAL NOT NULL,
+    "cliente_id" INTEGER NOT NULL,
+    "carro_id" INTEGER NOT NULL,
+    "valor_total" DOUBLE PRECISION NOT NULL,
+    "data_criacao" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "status" "Status" NOT NULL,
+    "descricao" TEXT,
+
+    CONSTRAINT "Orcamento_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Pagamento" (
+    "id" SERIAL NOT NULL,
+    "orcamento_id" INTEGER NOT NULL,
+    "valor_pago" DOUBLE PRECISION NOT NULL,
+    "data_pagamento" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "metodo_pagamento" TEXT NOT NULL,
+    "observacao" TEXT,
+
+    CONSTRAINT "Pagamento_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Usuario_email_key" ON "Usuario"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Carro_placa_key" ON "Carro"("placa");
+
+-- AddForeignKey
+ALTER TABLE "Carro" ADD CONSTRAINT "Carro_cliente_id_fkey" FOREIGN KEY ("cliente_id") REFERENCES "Usuario"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Orcamento" ADD CONSTRAINT "Orcamento_cliente_id_fkey" FOREIGN KEY ("cliente_id") REFERENCES "Usuario"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Orcamento" ADD CONSTRAINT "Orcamento_carro_id_fkey" FOREIGN KEY ("carro_id") REFERENCES "Carro"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Pagamento" ADD CONSTRAINT "Pagamento_orcamento_id_fkey" FOREIGN KEY ("orcamento_id") REFERENCES "Orcamento"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
